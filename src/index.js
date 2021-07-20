a12ecd39ec505609056f23e42221a433c2b0e071
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2021-07-20 10:46:40 +0400

    solve the task

--- src/index.js
+++ src/index.js
@@ -32,19 +32,56 @@ module.exports = function toReadable (number) {
      60: 'sixty',
      70: 'seventy',
      80: 'eighty',
-     90: 'ninety'
+     90: 'ninety',
+     2: 'twenty',
+     3: 'thirty',
+     4: 'forty',
+     5: 'fifty',
+     6: 'sixty',
+     7: 'seventy',
+     8: 'eighty',
+     9: 'ninety'
  }
 
+ const firstChar = Number(String(number)[0]);
+ const secondChar = Number(String(number)[1]);
+ const thirdChar = Number(String(number)[2]);
+ const substrChar = Number(String(number).substring(1));
+
+ if (number === 0) {
+     return 'zero';
+ }
  if (number < 10) {
      return units[number];
  }
  if (number < 20) {
     return teens[number];
  }
+ if (substrChar === 10) {
+    return units[firstChar] + ' ' + 'hundred' + ' ' + 'ten';
+}
  if (String(number).length === 2 && String(number)[1] === '0') {
     return dozens[number];
  }
+
  if (String(number).length === 3 && String(number)[1] === '0' && String(number)[2] === '0') {
-    return units[Number(String(number)[0])] + ' ' + 'hundred';
+    return units[firstChar] + ' ' + 'hundred';
  }
-}
+ if (String(number).length === 3 && String(number)[2] === '0') {
+    return units[firstChar] + ' ' + 'hundred' + ' ' + dozens[secondChar];
+ }
+ if (String(number).length === 3 && substrChar > 10 && substrChar < 20) {
+    return units[firstChar] + ' ' + 'hundred' + ' ' + teens[substrChar];
+ }
+
+
+ if (String(number).length === 2) {
+    return dozens[firstChar] + ' ' + units[secondChar];
+ }
+ if (String(number).length === 3) {
+     if (secondChar === 0) {
+         return units[firstChar] + ' ' + 'hundred' + ' ' + units[thirdChar];
+     }
+    return units[firstChar] + ' ' + 'hundred' + ' ' + dozens[secondChar] + ' ' + units[thirdChar];
+ }
+};
